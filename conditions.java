945a35bc0bbf93dddf1c54d89fa1f82aa819464a
Omar <jeffcostello70@protonmail.com>
2022-02-02 19:29:12 +0100

    FIrst java commit

---
+++ conditions.java
@@ -0,0 +1,21 @@
+public class conditions{
+
+public static void main(String []args){
+
+int age = 5;
+
+if (age >= 18) {
+
+System.out.println("You are an Adult!");
+
+} else {
+
+System.out.println("You are not an Adult!");
+
+}
+
+
+
+
+}
+}
